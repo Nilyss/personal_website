f10d74786b14a38524b2f3fd5830bc1d38436fe1
Nicolas DECRESSAC <101709161+Nilyss@users.noreply.github.com>
2022-09-08 18:46:36 +0200

    Init project
    Starting the first version of website

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+// styles
+import './_home.scss';
+
+// init Page
+export default function Home() {
+  // render the function in dom
+  return(<><h1>Home Page init</h1></>)
+}
---
+++ src/Pages/NotExistingPage/NotExistingPage.jsx
@@ -0,0 +1,8 @@
+// styles
+import './_notExistingPage.scss'
+
+// init Page
+export default function NotExistingPage() {
+  // render the function in dom
+  return(<><h1>404 Page</h1></>)
+}
